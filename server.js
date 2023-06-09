974814a42de6090f7d0354fb012a65e60a34f85f
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-06-09 08:27:24 +0530

    created api call for joining meetings

--- server.js
+++ server.js
@@ -24,6 +24,46 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
+//joining a new meeting
+app.get('/join', (req,res) => {
+    res.redirect(
+        url.format({
+            pathname :  `/join/${uuidv4()}`,
+            query: req.query
+        })
+    )
+})
+
+//For already existing meeting
+app.get('/joinold/:meetingid', (req, res) => {
+    res.redirect(
+        url.format({
+            pathname: req.params.meeting_id,
+            query: req.query
+        })
+    )
+})
+
+//Joining a meeting room
+app.get('/join/:rooms', (req,res) => {
+    res.render("room", {roomid: req.params.rooms, Myname: req.query.name})
+})
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomid, name, id) => {
+        socket.join(roomid);
+        socket.to(roomid).broadcast.emit("user-connected", name, id);
+
+        socket.on("tellName", (name) => {
+            socket.to(roomid).broadcast.emit("AddName", name);
+        });
+
+        socket.on("disconnect", (id)=> {
+            socket.to(roomid).broadcast.emit("user-disconnect", id)
+        })
+    })
+})
+
 server.listen(PORT, () => {
     console.log('Server is running');
 })
