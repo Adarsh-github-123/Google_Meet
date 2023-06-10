ffa0b75fa7a5aaf2f9dc50b5b124f2843c47c778
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-06-11 01:36:30 +0530

    updated main.js

--- static/main.js
+++ static/main.js
@@ -1,121 +1,118 @@
-const socket = io('/');
-const main__chat__window = document.getElementById('main__chat__window');
+const socket = io("/");
+const main__chat__window = document.getElementById("main__chat_window");
 const videoGrids = document.getElementById("video-grids");
 const myVideo = document.createElement("video");
 const chat = document.getElementById("chat");
+OtherUsername = "";
 
-otherUsername = "";
 myVideo.muted = true;
 
-//using jquery
 window.onload = () => {
     $(document).ready(function() {
         $("#getCodeModal").modal("show");
-    })
-}
+    });
+};
 
 var peer = new Peer(undefined, {
     path: "/peerjs",
     host: "/",
-    port: "3000"
-})
+    port: "3000",
+});
 
 let myVideoStream;
 const peers = {};
+var getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
 
-var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 
-getUserMedia({
-    video: true,
-    audio: true
-}).then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream, myname);
 
-    socket.on("user-connected", (username, id) => {
-        connectToNewUser(id, stream, username);
-        socket.emit("tellName", myname);
+navigator.mediaDevices
+    .getUserMedia({
+        video: true,
+        audio: true,
+    })
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream, myname);
+
+        socket.on("user-connected", (id, username) => {
+            console.log("userid:" + id);
+            connectToNewUser(id, stream, username);
+            socket.emit("tellName", myname);
+        });
+
+        socket.on("user-disconnected", (id) => {
+            console.log(peers);
+            if (peers[id]) peers[id].close();
+        });
     });
-
-    socket.on("user-disconnected", (id) => {
-        if(peers[id])
-            peers[id].close();
-    })
-});
-
 peer.on("call", (call) => {
-    getUserMedia({video: true, audio: true}, (stream) => {
-        call.answer(stream);
-        const video = document.createElement("video");
-        call.on("stream", (remoteStream) => {
-            addVideoStream(video, remoteStream, "RK");
-        })
-    })
-})
+    getUserMedia({ video: true, audio: true },
+        function(stream) {
+            call.answer(stream); // Answer the call with an A/V stream.
+            const video = document.createElement("video");
+            call.on("stream", function(remoteStream) {
+                addVideoStream(video, remoteStream, OtherUsername);
+            });
+        },
+        function(err) {
+            console.log("Failed to get local stream", err);
+        }
+    );
+});
 
 peer.on("open", (id) => {
-    socket.emit("join-room", roomId, myname, id);
+    socket.emit("join-room", roomId, id, myname);
 });
 
-peer.on("AddName", (username) =>{
-    otherUsername = username;
+
+socket.on("AddName", (username) => {
+    OtherUsername = username;
+    console.log(username);
 });
 
 const RemoveUnusedDivs = () => {
-    const alldivs = videoGrids.getElementsByTagName("div");
-    for(var i = 0; i < alldivs.length; i++) {
+    //
+    alldivs = videoGrids.getElementsByTagName("div");
+    for (var i = 0; i < alldivs.length; i++) {
         e = alldivs[i].getElementsByTagName("video").length;
-        if(e === 0) {
+        if (e == 0) {
             alldivs[i].remove();
         }
     }
-}
+};
 
 const connectToNewUser = (userId, streams, myname) => {
     const call = peer.call(userId, streams);
     const video = document.createElement("video");
     call.on("stream", (userVideoStream) => {
-        addVideoStream(video, userVideoStream, myname)
-    })
-
+        //       console.log(userVideoStream);
+        addVideoStream(video, userVideoStream, myname);
+    });
     call.on("close", () => {
         video.remove();
         RemoveUnusedDivs();
     });
-
     peers[userId] = call;
-}
+};
 
+const cancel = () => {
+    $("#getCodeModal").modal("hide");
+};
 
-const addVideoStream = (videoEl, stream, name) => {
-    videoEl.srcObject = stream;
-    videoEl.addEventListner("loadedmetadata", () => {
-        videoEl.play()
-    });
-
-    const h1 = document.createElement("h1");
-    const h1name = document.createTextNode(name);
-    h1.appendChild(h1name);
-
-    const videoGrid = document.createElement("div");
-    videoGrid.classList.add("video-grid");
-    videoGrid.appendChild(h1);
-    videoGrids.appendChild(videoGrid);
-    videoGrid.append(videoEl);
-
-    RemoveUnusedDivs();
-
-    let totalUsers = document.getElementsByTagName("video").length;
-    if(totalUsers > 1) {
-        for(let i = 0; i<totalUsers; i++){
-            document.getElementsByTagName("video")[i].style.width = 100/totalUsers + "%";
-        }
-    }
-}
+const copy = async() => {
+    const roomid = document.getElementById("roomid").innerText;
+    await navigator.clipboard.writeText("http://localhost:3030/join/" + roomid);
+};
+const invitebox = () => {
+    $("#getCodeModal").modal("show");
+};
 
 const muteUnmute = () => {
     const enabled = myVideoStream.getAudioTracks()[0].enabled;
-    if(enabled) {
+    if (enabled) {
         myVideoStream.getAudioTracks()[0].enabled = false;
         document.getElementById("mic").style.color = "red";
     } else {
@@ -126,11 +123,36 @@ const muteUnmute = () => {
 
 const VideomuteUnmute = () => {
     const enabled = myVideoStream.getVideoTracks()[0].enabled;
-    if(enabled) {
+    console.log(getUserMedia);
+    if (enabled) {
         myVideoStream.getVideoTracks()[0].enabled = false;
-        document.getElementById("mic").style.color = "red";
+        document.getElementById("video").style.color = "red";
     } else {
-        document.getElementById("mic").style.color = "white";
+        document.getElementById("video").style.color = "white";
         myVideoStream.getVideoTracks()[0].enabled = true;
     }
-}
+};
+
+
+const addVideoStream = (videoEl, stream, name) => {
+    videoEl.srcObject = stream;
+    videoEl.addEventListener("loadedmetadata", () => {
+        videoEl.play();
+    });
+    const h1 = document.createElement("h1");
+    const h1name = document.createTextNode(name);
+    h1.appendChild(h1name);
+    const videoGrid = document.createElement("div");
+    videoGrid.classList.add("video-grid");
+    videoGrid.appendChild(h1);
+    videoGrids.appendChild(videoGrid);
+    videoGrid.append(videoEl);
+    RemoveUnusedDivs();
+    let totalUsers = document.getElementsByTagName("video").length;
+    if (totalUsers > 1) {
+        for (let index = 0; index < totalUsers; index++) {
+            document.getElementsByTagName("video")[index].style.width =
+                100 / totalUsers + "%";
+        }
+    }
+};
